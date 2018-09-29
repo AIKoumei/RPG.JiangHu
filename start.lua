6dfd3813e575022eb14e2e5b6e318d8fcc7d2a28
zhm <koumei.rin@qq.com>
2018-09-29 20:12:28 +0800

    初始化

---
+++ start.lua
@@ -0,0 +1,15 @@
+require "base.base_class"
+require "base.class_to_file"
+require "base.config"
+require "base.override_function"
+require "base.global_function"
+require "util.print_util"
+require "util.table_util"
+require "util.file_util"
+require "test.test_one"
+
+function start()
+    FileUtil.Init()
+end
+
+start()
